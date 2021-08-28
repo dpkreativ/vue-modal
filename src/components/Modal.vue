3722c9420719a7850f0ec09785d41ef80c9f3aba
Divine Orji <dpkreativ@gmail.com>
2021-08-28 12:23:57 +0100

    add modal backdrop

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,5 +1,8 @@
 <template>
-  <div id="modal-backdrop">
+  <div
+    id="modal-backdrop"
+    class="fixed inset-0 bg-opacity-20 bg-black flex justify-center items-center"
+  >
     Hello! I'm gonna be a modal
   </div>
 </template>
